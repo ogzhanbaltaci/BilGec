7eb406acc34adaeb02e38cc409c03c3889477af4
Oguzhan Baltaci <ogzhan.baltaci@gmail.com>
2023-04-03 18:41:51 +0300

    Added jumping and climbing animations and cinemachine follow camera.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,19 +6,26 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float runSpeed = 10f;
     [SerializeField] float jumpSpeed = 5f;
+    [SerializeField] float climbSpeed = 5f;
     Vector2 moveInput;
     Rigidbody2D myRigidbody;
     Animator myAnimator;
+    BoxCollider2D myFeetCollider;
+    bool isGrounded;
+    float gravityScaleAtStart;
     void Start()
     {
         myRigidbody = GetComponent<Rigidbody2D>();
         myAnimator = GetComponent<Animator>();
+        myFeetCollider = GetComponent<BoxCollider2D>();
+        gravityScaleAtStart = myRigidbody.gravityScale;
     }
 
-        void Update()
+    void Update()
     {
         Run();
         FlipSprite();
+        ClimbLadder();
     }
 
     void OnMove(InputValue value)
@@ -27,12 +34,13 @@ public class PlayerMovement : MonoBehaviour
     }
     void OnJump(InputValue value)
     {
-        if(value.isPressed)
+        if(value.isPressed && isGrounded)
         {
             myRigidbody.velocity += new Vector2 (0f, jumpSpeed);
         }
         
         myAnimator.SetBool("isJumping", true);
+        
     }
     void Run()
     {
@@ -53,8 +61,35 @@ public class PlayerMovement : MonoBehaviour
         }
         
     }
-    public void OnLanding()
+    void ClimbLadder()
     {
-        myAnimator.SetBool("isJumping", false);
+        if(!myFeetCollider.IsTouchingLayers(LayerMask.GetMask("Climbing"))){
+            myRigidbody.gravityScale = gravityScaleAtStart;
+            myAnimator.SetBool("isClimbing", false);
+            return;
+        }
+        Vector2 climbVelocity = new Vector2 (myRigidbody.velocity.x, moveInput.y * climbSpeed);
+        myRigidbody.velocity = climbVelocity;
+        myRigidbody.gravityScale = 0;
+        bool playerHasVerticalSpeed = Mathf.Abs(myRigidbody.velocity.y) > Mathf.Epsilon;
+        myAnimator.SetBool("isClimbing", playerHasVerticalSpeed);
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+            myAnimator.SetBool("isJumping", false);
+            Debug.Log(isGrounded);
+
+        }
+    }
+    void OnCollisionExit2D(Collision2D Collider)
+    {
+        if (Collider.gameObject.CompareTag("Ground") && !myFeetCollider.IsTouchingLayers(LayerMask.GetMask("Ground")))
+        {
+            isGrounded = false;
+            Debug.Log(isGrounded);
+        }
     }
 }
